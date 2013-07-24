9cbebd10f32d0d36e9af1dd9ac9e41b3fdfb1aaf
Carey Metcalfe <pR0Ps.cm@gmail.com>
2013-07-23 23:06:41 -0400

    Add most of the functionality

--- tracks2cal.py
+++ tracks2cal.py
@@ -1,4 +1,4 @@
-#!/usr/lib/python
+#!/usr/bin/python
 
 import httplib2
 import logging
@@ -15,6 +15,11 @@ from oauth2client.tools import run
 
 from lxml import etree
 
+import datetime
+
+TIME_OUT_FMT = "%Y-%m-%dT%H:%M:%SZ"
+TIME_IN_FMT = "%Y-%m-%dT%H:%M:%S.%fZ"
+
 CLIENT_SECRETS = "client_secrets.json"
 
 FLOW = flow_from_clientsecrets(CLIENT_SECRETS,
@@ -23,6 +28,7 @@ FLOW = flow_from_clientsecrets(CLIENT_SECRETS,
       'https://www.googleapis.com/auth/calendar',
     ],
     message="Couldn't find client secrets file at %s" % (CLIENT_SECRETS,))
+
 
 class Tracks2Cal(object):
 
@@ -48,8 +54,6 @@ class Tracks2Cal(object):
         self.folder_name = folder_name
         self.cal_name = cal_name
         self.cal_id = self.get_calendar_id()
-    
-        #self.parse_kml_data(open("test.kml", "r").read())
 
     def get_calendar_id(self):
         """Get the calendar id to use when adding events"""
@@ -59,94 +63,131 @@ class Tracks2Cal(object):
             if x["summary"] == self.cal_name:
                 return x["id"]
         else:
-            print ("No calendar named '%s' found, creating one" % (self.cal_name,))
-            resp = self.cal_service.calendars().insert(body={"summary": self.cal_name}).execute()
-            return resp["id"]
+            logging.info("No calendar named '%s' found, creating one" % (self.cal_name,))
+
+            cal_data = {
+                "summary": self.cal_name
+            }
+            r = self.cal_service.calendars().insert(body=cal_data).execute()
+            return r["id"]
+
+    def _get_placemark_time(self, placemark, ns):
+        """Returns a datetime object representuing the time set in the placemark"""
+        text = placemark.find("{%s}TimeStamp" % ns).find("{%s}when" % ns).text
+        return datetime.datetime.strptime(text, TIME_IN_FMT)
 
     def parse_kml_data(self, data):
         """Parse the KML data and return a (start, end, coords, description) tuple"""
+
+        logging.debug("Parsing KML data")
+
         kml_root = etree.fromstring(data)
-        ns = kml_root.nsmap
 
-        print ns
+        # Get the default namespace
+        ns = kml_root.nsmap[None]
 
-        pms = kml_root.find("Document").findall("Placemark")
+        # Get the placemarks (start, tour, end)
+        placemarks = kml_root.find("{%s}Document" % ns).findall("{%s}Placemark" % ns)
 
-        print pms
-
-        start_time = pms[0].find("TimeStamp").find("when").text
-        end_time = pms[2].find("TimeStamp").find("when").text
-        desc = pms[2].find("description").text
+        # Get the data from the placemarks
+        # TODO: Use the 'styleURL' ("#start", "#track", "#end") instead of indecies
+        start_time = self._get_placemark_time(placemarks[0], ns)
+        end_time = self._get_placemark_time(placemarks[2], ns)
+        desc = placemarks[2].find("{%s}description" % ns).text
 
         # Convert "long,lat[,altitude]" to "lat,long"
-        temp = pms.find("Point").find("coordinates").text
+        temp = placemarks[0].find("{%s}Point" % ns).find("{%s}coordinates" % ns).text
         coords = ",".join(temp.split(",")[1::-1])
-
-        print start_time
-        print end_time
-        print coords
-        print desc
 
         return start_time, end_time, coords, desc
 
     def kml_file_data(self):
         """
         Generator for KML file data
-        
+
         Grabs the KML data from Google Drive and generates (filename, file_data) objects
         """
         try:
+            logging.debug("Opening the 'My Tracks' folder in Google Drive")
+
             # Get the "My Tracks" folder id
             folder = self.drive_service.files().list(q="mimeType='application/vnd.google-apps.folder' and title='%s'" % (self.folder_name,)).execute()["items"]
             if not folder:
-                print ("No '%s' folder found, exiting", (self.folder_name,))
+                logging.critical("No '%s' folder found in Google Drive, exiting", (self.folder_name,))
                 return
 
             folder_id = folder[0]["id"]
 
-            # Get the children of the my tracks folder
+            logging.debug("Getting kml files in the folder")
+
+            # Get the children of the My Tracks folder
+            # TODO: Pagination
             for x in self.drive_service.children().list(folderId=folder_id, q="mimeType='application/vnd.google-earth.kml+xml' and trashed=False").execute()["items"]:
                 file_ = self.drive_service.files().get(fileId=x["id"]).execute()
+
+                filename_ext = file_["title"]
+
+                # Remove extension of filename
+                filename = "".join(filename_ext.split(".")[:-1])
+
+                logging.debug("Downloading data from '%s'" % (filename_ext,))
 
                 # Download the data of the kml file
                 r, data = self.drive_service._http.request(file_["downloadUrl"])
                 if r.status == 200:
-                    yield file_["title"], data
+                    yield filename, data
                 else:
-                    print ("Error occurred downloading KML file: %s" % (str(r),))
-                
+                    logging.warning("Error occurred downloading KML file: %s" % (str(r),))
 
         except (errors.HttpError, KeyError), e:
-            print ("Error occurred: %s" % (str(e),))
+            logging.critical("Error occurred: %s" % (str(e),))
             return
 
     def event_exists(self, title, start, end):
         """Checks if the event already exists in Google Calendar"""
 
-        # TODO?: Add to start, subtract from end
-        self.cal_service.events().list(timeMax=start, timeMin=end).execute()
+        logging.debug("Checking if event '%s' exists..." % (title,))
+
+        # Fuzz the times a bit to take rounding into account
+        fuzz = datetime.timedelta(seconds=1)
+        timeMin = (start + fuzz).strftime(TIME_OUT_FMT)
+        timeMax = (start + 2 * fuzz).strftime(TIME_OUT_FMT)
+
+        # Get all events that exist in the start+fuzz to start+2*fuzz window
+        r = self.cal_service.events().list(calendarId=self.cal_id, timeMin=timeMin, timeMax=timeMax).execute()["items"]
+
+        # If any of the events at this time have the same title as the event, it already exists
+        if [x for x in r if x["summary"] == title]:
+            logging.debug("Event exists")
+            return True
+
+        logging.debug("Event doesn't exist")
+        return False
 
     def add_event(self, title, start, end, loc="", desc=""):
         """Adds an event to Google Calendar"""
 
-        return
+        logging.debug("Adding event '%s' to calendar" % (title,))
+
+        # Set up event properties
         event = {
             "summary": title,
             "location": loc,
             "description": desc,
-            "start": dict(datetime=start), #convert to string, ex: '2011-06-03T10:00:00.000-07:00'
-            "end": dict(datetime=end), #convert to string, ex: '2011-06-03T10:00:00.000-07:00'
+            "start": dict(dateTime=start.strftime(TIME_OUT_FMT)),
+            "end": dict(dateTime=end.strftime(TIME_OUT_FMT)),
         }
 
-        self.cal_service.events().insert(calendarDd=self.cal_id, body=event)
+        r = self.cal_service.events().insert(calendarId=self.cal_id, body=event).execute()
+
+        logging.debug("Event '%s' created" % (r["summary"],))
 
     def run(self):
         """
         Finds all MyTracks files in Google Drive and creates events in
         Google Calendar according to their properties
         """
-        return
-        for filename, file_data in self.kml_file_data(drive_service):
+        for filename, file_data in self.kml_file_data():
             start, end, coords, desc = self.parse_kml_data(file_data)
             if not self.event_exists(filename, start, end):
                 self.add_event(filename, start, end, coords, desc)
@@ -156,11 +197,11 @@ def main():
     try:
         Tracks2Cal(cal_name="TEST_CAL").run()
     except AccessTokenRefreshError:
-        print ("The credentials have been revoked or expired, please re-run the application to re-authorize")
+        logging.critical("The credentials have been revoked or expired, please re-run the application to re-authorize")
 
 
 if __name__ == '__main__':
     # https://google-api-client-libraries.appspot.com/documentation/drive/v2/python/latest/
     # https://google-api-client-libraries.appspot.com/documentation/calendar/v3/python/latest/
-    
+    logging.basicConfig(format="[%(asctime)s][%(levelname)s]: %(message)s", level=logging.DEBUG)
     main()
